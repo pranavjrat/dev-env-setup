d2a9eea565e268d8ec21aefddecf9fc8428c5083
pranavjrat <pranavjrat@gmail.com>
2026-05-28 18:59:29 +0530

    ghostty +list-themes

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,32 +1,32 @@
 return {
-    {
-      'nvim-telescope/telescope.nvim',
-      -- tag = '0.1.8',   <-- remove this line
-      branch = 'master',
-      dependencies = { 'nvim-lua/plenary.nvim' },
-      config = function()
-        local builtin = require("telescope.builtin")
-        vim.keymap.set('n','<leader>pf', builtin.find_files, {})
-        vim.keymap.set('n','<C-p>', builtin.git_files, {})
-        vim.keymap.set('n','<leader>ps', builtin.live_grep, {})
-      end
-    },
-    {
-      "nvim-telescope/telescope-ui-select.nvim",
-      config = function()
-        require("telescope").setup {
-          defaults = {
-            preview = {
-              treesitter = false,
-            },
-          },
-          extensions = {
-            ["ui-select"] = {
-              require("telescope.themes").get_dropdown{}
-            }
-          }
-        }
-        require("telescope").load_extension("ui-select")
-      end
-    }
-  }
+	{
+		"nvim-telescope/telescope.nvim",
+		-- tag = '0.1.8',   <-- remove this line
+		branch = "master",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		config = function()
+			local builtin = require("telescope.builtin")
+			vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
+			vim.keymap.set("n", "<C-p>", builtin.git_files, {})
+			vim.keymap.set("n", "<leader>ps", builtin.live_grep, {})
+		end,
+	},
+	{
+		"nvim-telescope/telescope-ui-select.nvim",
+		config = function()
+			require("telescope").setup({
+				defaults = {
+					preview = {
+						treesitter = false,
+					},
+				},
+				extensions = {
+					["ui-select"] = {
+						require("telescope.themes").get_dropdown({}),
+					},
+				},
+			})
+			require("telescope").load_extension("ui-select")
+		end,
+	},
+}
--- .config/nvim/lua/set.lua
+++ .config/nvim/lua/set.lua
@@ -39,8 +39,8 @@ vim.opt.mouse = "nv"
 
 -- Performance optimizations
 vim.opt.lazyredraw = true -- Don't redraw during macros
-vim.opt.synmaxcol = 200 -- Limit syntax highlighting for long lines
-vim.opt.timeoutlen = 500 -- Faster timeout for key sequences
-vim.opt.ttimeoutlen = 10 -- Faster timeout for escape sequences
+vim.opt.synmaxcol = 200   -- Limit syntax highlighting for long lines
+vim.opt.timeoutlen = 500  -- Faster timeout for key sequences
+vim.opt.ttimeoutlen = 10  -- Faster timeout for escape sequences
 
-vim.opt.cmdheight = 0
+vim.opt.cmdheight = 1

d9c1c234b0052ea0aa502c54a222317684286a0b
pranavjrat <pranavjrat@gmail.com>
2026-05-31 04:31:26 +0530

    bwhold the tmux-noteier

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,49 +1,49 @@
 return {
-  { "chrisbra/sudoedit.vim" },
-  {
-    "GnikDroy/projections.nvim",
-    branch = "pre_release",
-    config = function()
-      require("projections").setup({
-        workspaces = {
-          { "/home/heisenberg/Documents/personal", { ".git" } },
-          { "/home/heisenberg/Documents/projects", { ".git" } },
-        },
-      })
+	{ "chrisbra/sudoedit.vim" },
+	{
+		"GnikDroy/projections.nvim",
+		branch = "pre_release",
+		config = function()
+			require("projections").setup({
+				workspaces = {
+					{ "/home/heisenberg/Documents/personal", { ".git" } },
+					{ "/home/heisenberg/Documents/projects", { ".git" } },
+				},
+			})
 
-      -- Telescope integration
-      require("telescope").load_extension("projections")
-      vim.keymap.set("n", "<leader>fp", function()
-        vim.cmd("Telescope projections")
-      end)
+			-- Telescope integration
+			require("telescope").load_extension("projections")
+			vim.keymap.set("n", "<leader>fp", function()
+				vim.cmd("Telescope projections")
+			end)
 
-      -- Autostore session on VimExit
-      local Session = require("projections.session")
-      vim.api.nvim_create_autocmd({ "VimLeavePre" }, {
-        callback = function()
-          Session.store(vim.loop.cwd())
-        end,
-      })
+			-- Autostore session on VimExit
+			local Session = require("projections.session")
+			vim.api.nvim_create_autocmd({ "VimLeavePre" }, {
+				callback = function()
+					Session.store(vim.loop.cwd())
+				end,
+			})
 
-      -- Switch to project if vim was started in a project dir
-      local switcher = require("projections.switcher")
-      vim.api.nvim_create_autocmd({ "VimEnter" }, {
-        callback = function()
-          if vim.fn.argc() == 0 then
-            switcher.switch(vim.loop.cwd())
-          end
-        end,
-      })
-    end,
-  },
-  {
-    "akinsho/toggleterm.nvim",
-    version = "*",
-    config = function()
-      require("toggleterm").setup({
-        size = 40,
-        direction = "vertical",
-      })
-    end,
-  },
+			-- Switch to project if vim was started in a project dir
+			local switcher = require("projections.switcher")
+			vim.api.nvim_create_autocmd({ "VimEnter" }, {
+				callback = function()
+					if vim.fn.argc() == 0 then
+						switcher.switch(vim.loop.cwd())
+					end
+				end,
+			})
+		end,
+	},
+	{
+		"akinsho/toggleterm.nvim",
+		version = "*",
+		config = function()
+			require("toggleterm").setup({
+				size = 40,
+				direction = "vertical",
+			})
+		end,
+	},
 }
--- .config/nvim/lua/plugins/copilot.lua
+++ .config/nvim/lua/plugins/copilot.lua
@@ -1,24 +1,24 @@
 return {
-	"github/copilot.vim",
-	lazy = false,
-	config = function()
-		vim.g.copilot_enabled = false
-		vim.g.copilot_no_tab_map = true
-		vim.g.copilot_assume_mapped = true
-		vim.keymap.set("n", "<leader>co", function()
-			if vim.g.copilot_enabled == 0 then
-				vim.cmd("Copilot enable")
-				print("Copilot enabled")
-			else
-				vim.cmd("Copilot disable")
-				print("Copilot disabled")
-			end
-		end, { desc = "Toggle Copilot" })
-		-- Accept suggestion with Ctrl-Y in insert mode
-		vim.cmd([[imap <silent><script><expr> <C-Y> copilot#Accept("\<CR>")]])
+  "github/copilot.vim",
+  lazy = false,
+  config = function()
+    vim.g.copilot_enabled = true
+    vim.g.copilot_no_tab_map = true
+    vim.g.copilot_assume_mapped = true
+    vim.keymap.set("n", "<leader>co", function()
+      if vim.g.copilot_enabled == 0 then
+        vim.cmd("Copilot enable")
+        print("Copilot enabled")
+      else
+        vim.cmd("Copilot disable")
+        print("Copilot disabled")
+      end
+    end, { desc = "Toggle Copilot" })
+    -- Accept suggestion with Ctrl-Y in insert mode
+    vim.cmd([[imap <silent><script><expr> <C-Y> copilot#Accept("\<CR>")]])
 
-		-- cycle through suggestions Alt-[ in insert mode
-		vim.cmd([[imap <silent> <M-]> <Plug>(copilot-next)]])
-		vim.cmd([[imap <silent> <M-[> <Plug>(copilot-previous)]])
-	end,
+    -- cycle through suggestions Alt-[ in insert mode
+    vim.cmd([[imap <silent> <M-]> <Plug>(copilot-next)]])
+    vim.cmd([[imap <silent> <M-[> <Plug>(copilot-previous)]])
+  end,
 }
